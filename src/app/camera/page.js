108c622e37c728867e5ac7d38c422c263ad85260
derryharyanto <derryharyanto2@gmail.com>
2023-10-24 22:15:36 +0700

    init commit

---
+++ src/app/camera/page.js
@@ -0,0 +1,75 @@
+"use client";
+
+import React, { useState, useRef, useContext } from "react";
+import { Button, Grid } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { QrReader } from "react-qr-reader";
+import { useAtom } from "jotai";
+import { AllUserData } from "../storing/userData";
+
+function Camera() {
+  const webcamRef = useRef(null);
+  const qrReaderRef = useRef(null);
+  const [showCamera, setShowCamera] = useState(false);
+  const router = useRouter();
+  const [data, setData] = useAtom(AllUserData);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      webcamRef.current = stream; // Assign the stream to the video element
+      setShowCamera(true);
+    } catch (error) {
+      console.log("Error accessing camera");
+    }
+  };
+
+  useEffect(() => {
+    startCamera();
+  }, []);
+
+  const stopCamera = () => {
+    if (webcamRef.current && showCamera) {
+      webcamRef.current.getTracks().forEach((track) => track.stop());
+      webcamRef.current = null;
+      setShowCamera(false);
+    }
+    router.push("/");
+  };
+
+  const convertObject = (value) => {
+    const temp = JSON.parse(value);
+    console.log("🚀 ~ file: page.js:56 ~ convertObject ~ temp:", temp.user);
+    setData((prev) => [...prev, temp.user]);
+  };
+
+  return (
+    <div>
+      <h1>Camera Page</h1>
+      <Grid item xs={12}>
+        {showCamera && (
+          <QrReader
+            ref={qrReaderRef} // Assign the ref to QrReader
+            onResult={(result, error) => {
+              console.log("cek " + result?.text + " error " + error);
+
+              if (!!result) {
+                stopCamera();
+                convertObject(result?.text);
+                qrReaderRef.current.stop();
+              }
+            }}
+            //this is facing mode : "environment " it will open backcamera of the smartphone and if not found will
+            // open the front camera
+            constraints={{ facingMode: "environment" }}
+            style={{ width: "40%", height: "40%" }}
+          />
+        )}
+      </Grid>
+    </div>
+  );
+}
+export default Camera;
---
+++ src/app/storing/userData.js
@@ -0,0 +1,5 @@
+import { Atom, atom } from "jotai";
+
+const AllUserData = atom([]);
+
+export { AllUserData };
